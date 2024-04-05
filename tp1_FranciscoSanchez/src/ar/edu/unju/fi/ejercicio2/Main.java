13ed60297b4769754c8f905ef5d1d9c7bf4be6f5
PC <PC@DESKTOP-TQFA51R>
2024-04-04 22:59:10 -0300

    Responsable: Francisco Ignacio Sanchez user: Francisco370 Descripcion: Se ha actualizado el trabajo al repositorio. Se actualiza el repositorio con la clase Close #1

---
+++ tp1_FranciscoSanchez/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,21 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String nombrePais = "Argentina";
+		int edadPersona = 22;
+		float alturaEdificio = 2.4f;
+		double precioProducto = 984.50;
+		String numTelefono = "3884652736";
+		double calcCoseno = 0.877583;
+		
+		System.out.println("Nombre de Pais: "+nombrePais);
+		System.out.println("Edad de una persona: "+edadPersona);
+		System.out.println("Altura de un Edificio: "+alturaEdificio);
+		System.out.println("Precio de un Producto: "+precioProducto);
+		System.out.println("Numero de Telefono: "+numTelefono);
+		System.out.println("Coseno de 0.5 es igual a "+calcCoseno);
+	}
+
+}
